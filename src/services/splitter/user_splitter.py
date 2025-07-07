8292cb9c09caafab117b8a3ca5badf7622c1d20e
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-07-07 19:32:09 +0100

    dipatch and splitter service updated

--- src/services/splitter/user_splitter.py
+++ src/services/splitter/user_splitter.py
@@ -1,13 +1,17 @@
 from supabase import Client, create_client
 from core.config import SUPABASE_KEY, SUPABASE_URL
 from datetime import timedelta, datetime
+from argparse import ArgumentParser
 
-LIMIT = 300
+parser = ArgumentParser()
+parser.add_argument("--limit", type=int, required=True)
+args = parser.parse_args()
+
 NEXT_30_DAYS = datetime.now() + timedelta(30)
 START_DATE = datetime.now()
 
 
-def share_creator(creator: str):
+def share_creator(creator: str, limit: int):
     """
     Share the creator among users
 
@@ -30,7 +34,7 @@ def share_creator(creator: str):
     # go through the users and assign to valid users
     added = 0
     for record in response.data:
-        if added == LIMIT:
+        if added == limit:
             print("Limit reached")
             break
         past_creators = record.get("past_creators")
@@ -50,8 +54,9 @@ def main():
     for creator in response.data:
         creator_username = creator["creator"]
         print(f"creator - {creator_username}")
-        share_creator(creator_username)
+        share_creator(creator_username, args.limit)
 
 
 if __name__ == "__main__":
+    print(args.limit + 10)
     main()
