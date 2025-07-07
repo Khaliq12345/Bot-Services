a9e23841a5749f34a6bc96fbcfc72c63ed594889
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-07-07 17:31:26 +0100

    split entry point edited

---
+++ src/services/splitter/server.py
@@ -0,0 +1,9 @@
+import http.server
+import socketserver
+
+PORT = 8111  # Change to your desired port
+
+Handler = http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
